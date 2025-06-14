5151951695059e85d1b27bd71b54b2c6b2083ff5
ishwar2611104 <f20230736@pilani.bits-pilani.ac.in>
2025-06-14 22:08:37 +0530

    Create SRLatch.v

---
+++ Task1/question7/SRLatch.v
@@ -0,0 +1,15 @@
+module SRLatch (
+    input S,
+    input R,
+    output reg Q
+);
+    always @ (S or R) begin
+        if (S && ~R)
+            Q = 1;
+        else if (~S && R)
+            Q = 0;
+        else if (~S && ~R)
+            Q = Q; // Hold
+        // S=1 and R=1 is invalid: do nothing here (or could handle separately)
+    end
+endmodule
