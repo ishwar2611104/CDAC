323e07466bcd67a70c0c9db59e47a2f408aeacea
ishwar2611104 <f20230736@pilani.bits-pilani.ac.in>
2025-06-14 21:41:04 +0530

    Create 4bitfulladdernonripple.v

---
+++ Task1/question5/4bitfulladdernonripple.v
@@ -0,0 +1,11 @@
+module fourbitfulladderNonRipple(
+    input [3:0] a,
+    input [3:0] b,
+    input cin,
+    output reg [3:0] sum,
+    output reg cout
+);
+    always @ (a or b or cin) begin
+        {cout, sum} = a + b + cin;
+    end
+endmodule
